fa83ecfbe08a329dbcbec528102c9e58d79af140
slac1n <email@github.com>
2024-11-12 13:39:05 +0100

    feat: Remove python setting in pyrigth

--- lua/niclas/plugins/lsp/lspconfig.lua
+++ lua/niclas/plugins/lsp/lspconfig.lua
@@ -109,11 +109,11 @@ return {
 				lspconfig["pyright"].setup({
 					capabilities = capabilities,
 					on_attach = on_attach,
-					settings = {
-						python = {
-							pythonPath = vim.fn.exepath("python"),
-						},
-					},
+					-- settings = {
+					-- 	python = {
+					-- 		pythonPath = vim.fn.exepath("python"),
+					-- 	},
+					-- },
 				})
 			end,
 		})
